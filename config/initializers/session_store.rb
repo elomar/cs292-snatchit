af6ddd13c278d539a5607dc553d736e6f8c04d6d
Elomar Souza <elomar@elomar.me>
2013-04-23 18:07:17 -0500

    upgrade cookie store to rails4 github

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Snatchit::Application.config.session_store :encrypted_cookie_store, key: '_snatchit_session'
+Snatchit::Application.config.session_store :cookie_store, key: '_snatchit_session'
