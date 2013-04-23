6443bb0c34888ed4b021255dbee5cc08536f4733
Elomar Souza <elomar@elomar.me>
2013-04-23 14:14:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snatchit::Application.config.secret_key_base = '3f683a4c5b134254ab38964928ed75a5d20c61937380d7c95b76f7a17c5ee9ee58a89ca471389e7e9e2d83fef9060a959969cdd2e8fc8a5eeedbd43dc5fea879'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snatchit::Application.config.session_store :encrypted_cookie_store, key: '_snatchit_session'
