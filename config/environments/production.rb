5302b35d97d57f9486eb92cd6ad13468f2aeb8dc
Elomar Souza <elomar@elomar.me>
2013-04-23 19:23:39 -0500

    bypass heroku assets issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,8 @@ Snatchit::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # bypassing https://github.com/heroku/heroku-buildpack-ruby/pull/74
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
