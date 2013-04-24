e3c09327d844c38c3ba9135440b68f6641d85ca9
Elomar Souza <elomar@elomar.me>
2013-04-23 19:15:49 -0500

    has to bid higher than current bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,6 +1,7 @@
 class Bid < ActiveRecord::Base
   belongs_to :product
   validates :value, :name, :email, presence: true
+  validates :value, numericality: { greater_than: -> (bid) { bid.product.current_value} }
 
   def previous
     product.bids.order("value").where("id != ?", id).last
